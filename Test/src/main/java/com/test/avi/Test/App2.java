3ee8584c412a87146e2064b20b10203d413a8927
AvinashSingh <AvinashSingh@DESKTOP-D6T140M>
2019-06-02 11:47:09 +0530

    changes pushed to develop

--- Test/src/main/java/com/test/avi/Test/App2.java
+++ Test/src/main/java/com/test/avi/Test/App2.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Avinash singh rajwat!" );
+        System.out.println( "Hello Avinash singh Rajawat!" );
     }
 }
