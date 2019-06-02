d2e12c424ea79bc6316b9582c5053fc191970649
AvinashSingh <AvinashSingh@DESKTOP-D6T140M>
2019-06-02 12:20:26 +0530

    add

--- Test/src/main/java/com/test/avi/Test/App2.java
+++ Test/src/main/java/com/test/avi/Test/App2.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Avinash singh Rajawattt!" );
+        System.out.println( "Hello Avinash singh Rajawat!" );
     }
 }
