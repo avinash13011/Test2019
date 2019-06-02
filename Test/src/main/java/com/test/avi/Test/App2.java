6bac85b1207bd0c839a24484bfc4ecf04ca6dc41
AvinashSingh <AvinashSingh@DESKTOP-D6T140M>
2019-06-02 12:17:26 +0530

    git add

--- Test/src/main/java/com/test/avi/Test/App2.java
+++ Test/src/main/java/com/test/avi/Test/App2.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Avinash singh Rajawatt!" );
+        System.out.println( "Hello Avinash singh Rajawattt!" );
     }
 }
